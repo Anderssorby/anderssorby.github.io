dc0f3ec43ad1e02fb33353f98db1e4dae879e009
Anders Christiansen Sørby <anders@sorby.xyz>
2020-07-23 21:00:39 +0200

    Migrate to hakyll

--- index.md
+++ index.md
@@ -2,11 +2,10 @@
 layout: default
 permalink: /
 ---
-<h1>Welcome to my homepage</h1>
 
-It might someday be full of interesting posts about various topics. See my [CV here](/files/English_CV.pdf).
+This might someday be full of interesting posts about various topics. See my [CV here](/files/English_CV.pdf).
 
-Topics I'm interested in:
+### Topics I'm interested in:
 
  * Deep learing and AI
  * Machine learning and Data Science in general
@@ -16,13 +15,4 @@ Topics I'm interested in:
  * Using knowledge to make the world a better place for everyone.
 
 I'm also an eager reader of the webcomics [Existential Comics](https://existentialcomics.com) and [SMBC - Saturday Morning Breakfast Cereal](https://smbc-comics.com).
-
-<h1>Posts</h1>
-<ul>
-  {% for post in site.posts %}
-    <li>
-      <a href="{{ post.url }}">{{ post.title }}</a>
-    </li>
-  {% endfor %}
-</ul>
 
---
+++ site.hs
@@ -0,0 +1,86 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid                    ( mappend )
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+  match "images/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "files/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "static/*/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match (fromList ["about.md"]) $ do
+    route $ setExtension "html"
+    compile
+      $   pandocCompiler
+      >>= loadAndApplyTemplate "templates/page.html"    siteCtx
+      >>= loadAndApplyTemplate "templates/default.html" siteCtx
+      >>= relativizeUrls
+
+  match "posts/*" $ do
+    route $ setExtension "html"
+    compile
+      $   pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html"    postCtx
+      >>= loadAndApplyTemplate "templates/default.html" postCtx
+      >>= relativizeUrls
+
+  create ["archive.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let archiveCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` constField "title" "Archive"
+              `mappend` siteCtx
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+        >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+        >>= relativizeUrls
+
+  match "index.md" $ do
+    compile $ pandocCompiler
+
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let indexCtx =
+            listField "posts" postCtx (return posts)
+              `mappend` field "index_text" (\_ -> loadBody "index.md")
+              `mappend` constField "title" "Anders C. Sørby"
+              `mappend` siteCtx
+      getResourceBody
+        >>= applyAsTemplate indexCtx
+        >>= loadAndApplyTemplate "templates/default.html" indexCtx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y" `mappend` siteCtx
+
+siteCtx :: Context String
+siteCtx =
+  constField "baseurl" ""
+    `mappend` constField "site_description" "My homepage"
+    `mappend` constField "twitter_username" "anders_sorby"
+    `mappend` constField "github_username"  "anderssorby"
+    `mappend` constField "header_img"       "sky.jpg"
+    `mappend` constField
+                "linkedin_url"
+                "https://www.linkedin.com/in/anders-christiansen-s%C3%B8rby/"
+    `mappend` defaultContext
